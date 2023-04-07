13b9940dc160cca9c2a68bef0f083a8ac2bc01b0
Benjamin <themonsterhidingunderyourbed@gmail.com>
2023-04-07 11:33:59 -0700

    added assignment files

--- server.py
+++ server.py
@@ -8,7 +8,8 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
-    return render_template("checkout.html")
+    print(f"Charging {request.form['first_name']} {request.form['last_name']} for {int(request.form['strawberry']) + int(request.form['raspberry']) + int(request.form['apple'])} fruits")
+    return render_template("checkout.html", strawberry = request.form['strawberry'], raspberry = request.form['raspberry'], apple = request.form['apple'], first_name = request.form['first_name'], last_name = request.form['last_name'], student_id = request.form['student_id'])
 
 @app.route('/fruits')         
 def fruits():
